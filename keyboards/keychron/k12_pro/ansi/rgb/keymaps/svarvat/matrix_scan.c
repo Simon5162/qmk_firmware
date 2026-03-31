ad7d549cbb20acd2c2c38d991134d12b52569ac6
Simon Varvat <simon.varvat-ext@ixxitechside.com>
2026-03-31 16:57:18 +0200

    #WORK: Upgrade keymap part 24

--- keyboards/keychron/k12_pro/ansi/rgb/keymaps/svarvat/matrix_scan.c
+++ keyboards/keychron/k12_pro/ansi/rgb/keymaps/svarvat/matrix_scan.c
@@ -1,138 +1,113 @@
 
-uint16_t scrollDelayTimer;
+uint16_t floodTimerX3 = 0;
+uint16_t floodTimerX2 = 0;
+uint16_t floodTimerX1 = 0;
+uint16_t floodKeyCodeIntervalX3 = 80;
+uint16_t floodKeyCodeIntervalX2 = 150;
+uint16_t floodScrollIntervalX1 = 40;
+uint16_t floodScrollIntervalX2 = 100;
+uint16_t floodScrollIntervalX3 = 100;
+uint16_t floodMouseIntervalX1 = 10;
+uint16_t floodMouseIntervalX2 = 100;
+uint16_t floodMouseIntervalX3 = 100;
+uint16_t floodBlueToothInterval = 500;
 
 extern bool bt_wake_pending;
 extern uint32_t bt_wake_timer;
 
+extern bool isFloodUpOn;
+extern bool isFloodDownOn;
+extern bool isFloodLeftOn;
+extern bool isFloodRightOn;
+extern bool isFloodDelOn;
+extern bool isFloodEntOn;
+extern bool isFloodBspcOn;
+
+static void move_mouse(int8_t x, int8_t y) {
+    report_mouse_t r = pointing_device_get_report();
+    r.x = x;
+    r.y = y;
+    pointing_device_set_report(r);
+    pointing_device_send();
+}
+
+static void scroll_mouse(int8_t v, int8_t h) {
+    report_mouse_t r = pointing_device_get_report();
+    r.v = v;
+    r.h = h;
+    pointing_device_set_report(r);
+    pointing_device_send();
+}
+
+static void dispatch_flood_keys(void) {
+    if (isFloodUpOn)    tap_code16(KC_UP);
+    if (isFloodDownOn)  tap_code16(KC_DOWN);
+    if (isFloodLeftOn)  tap_code16(KC_LEFT);
+    if (isFloodRightOn) tap_code16(KC_RIGHT);
+    if (isFloodDelOn)   tap_code16(KC_DEL);
+    if (isFloodEntOn)   tap_code16(KC_ENT);
+    if (isFloodBspcOn)  tap_code16(KC_BSPC);
+}
+
 void matrix_scan_user(void) {
-    if (bt_wake_pending && timer_elapsed32(bt_wake_timer) > 500) {
+    if (bt_wake_pending && timer_elapsed32(bt_wake_timer) > floodBlueToothInterval) {
         bt_wake_pending = false;
         tap_code16(KC_RSFT);
     }
     if (IS_LAYER_ON(LA_MOUSE)) {
-        if (isMouseX4Started) {
-            if (mouseRight) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = 8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (mouseLeft) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = -8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-            if (mouseDown) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = 8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (mouseUp) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = -8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-        } else if (isMouseX2Started) {
-            if (mouseDown) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = 3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (mouseUp) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = -3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-            if (mouseRight) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = 3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (mouseLeft) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = -3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-        } else if (isMouseX1Started && (mouseDown || mouseLeft || mouseUp || mouseRight)) {
-            if (timer_elapsed(scrollDelayTimer) > 10) {
-                if (mouseDown || mouseUp) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.y = mouseDown ? 1 : -1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                }
-                if (mouseRight || mouseLeft) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.x = mouseRight ? 1 : -1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                }
-                scrollDelayTimer = timer_read();
-            }
-        }
-        if (isScrollX4Started) {
-            if (scrollDown || scrollUp) {
-                if (timer_elapsed(scrollDelayTimer) > 100) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.v = scrollUp ? 20 : -20;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    scrollDelayTimer = timer_read();
+        if (IS_LAYER_ON(LA_LTHUMBEMO)) {
+            int8_t mx = isMouseRightOn ? 8 : (isMouseLeftOn ? -8 : 0);
+            int8_t my = isMouseDownOn  ? 8 : (isMouseUpOn   ? -8 : 0);
+            if (mx || my) move_mouse(mx, my);
+
+            if (isScrollDownOn || isScrollUpOn || isScrollLeftOn || isScrollRightOn) {
+                if (timer_elapsed(floodTimerX3) > floodScrollIntervalX3) {
+                    scroll_mouse(isScrollUpOn   ? 20 : (isScrollDownOn  ? -20 : 0),
+                                 isScrollRightOn ? 20 : (isScrollLeftOn ? -20 : 0));
+                    floodTimerX3 = timer_read();
                 }
             }
-            if (scrollLeft || scrollRight) {
-                if (timer_elapsed(scrollDelayTimer) > 100) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.h = scrollLeft ? -20 : 20;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    scrollDelayTimer = timer_read();
+            if (isFloodUpOn || isFloodDownOn || isFloodLeftOn || isFloodRightOn
+            || isFloodDelOn || isFloodEntOn || isFloodBspcOn) {
+                if (timer_elapsed(floodTimerX3) > floodKeyCodeIntervalX3) {
+                    dispatch_flood_keys();
+                    floodTimerX3 = timer_read();
                 }
             }
-        } else if (isScrollX2Started) {
-            if (scrollDown || scrollUp) {
-                if (timer_elapsed(scrollDelayTimer) > 100) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.v = scrollUp ? 10 : -10;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    scrollDelayTimer = timer_read();
+        } else if (IS_LAYER_ON(LA_LTHUMBDMO)) {
+            int8_t mx = isMouseRightOn ? 3 : (isMouseLeftOn ? -3 : 0);
+            int8_t my = isMouseDownOn  ? 3 : (isMouseUpOn   ? -3 : 0);
+            if (mx || my) move_mouse(mx, my);
+
+            if (isScrollDownOn || isScrollUpOn || isScrollLeftOn || isScrollRightOn) {
+                if (timer_elapsed(floodTimerX2) > floodScrollIntervalX2) {
+                    scroll_mouse(isScrollUpOn   ? 10 : (isScrollDownOn  ? -10 : 0),
+                                 isScrollRightOn ? 10 : (isScrollLeftOn ? -10 : 0));
+                    floodTimerX2 = timer_read();
                 }
             }
-            if (scrollLeft || scrollRight) {
-                if (timer_elapsed(scrollDelayTimer) > 100) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.h = scrollLeft ? -10 : 10;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    scrollDelayTimer = timer_read();
+            if (isFloodUpOn || isFloodDownOn || isFloodLeftOn || isFloodRightOn
+            || isFloodDelOn || isFloodEntOn || isFloodBspcOn) {
+                if (timer_elapsed(floodTimerX2) > floodKeyCodeIntervalX2) {
+                    dispatch_flood_keys();
+                    floodTimerX2 = timer_read();
                 }
             }
-        } else
-        if (isScrollX1Started) {
-            if (scrollDown || scrollUp) {
-                if (timer_elapsed(scrollDelayTimer) > 40) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.v = scrollUp ? 1 : -1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    scrollDelayTimer = timer_read();
+        } else {
+            if (isMouseRightOn || isMouseLeftOn || isMouseDownOn || isMouseUpOn) {
+                if (timer_elapsed(floodTimerX1) > floodMouseIntervalX1) {
+                    move_mouse(isMouseRightOn ? 1 : (isMouseLeftOn ? -1 : 0),
+                               isMouseDownOn  ? 1 : (isMouseUpOn   ? -1 : 0));
+                    floodTimerX1 = timer_read();
                 }
             }
-            if (scrollLeft || scrollRight) {
-                if (timer_elapsed(scrollDelayTimer) > 40) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.h = scrollLeft ? -1 : 1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    scrollDelayTimer = timer_read();
+            if (isScrollDownOn || isScrollUpOn || isScrollLeftOn || isScrollRightOn) {
+                if (timer_elapsed(floodTimerX1) > floodScrollIntervalX1) {
+                    scroll_mouse(isScrollUpOn   ? 1 : (isScrollDownOn  ? -1 : 0),
+                                 isScrollRightOn ? 1 : (isScrollLeftOn ? -1 : 0));
+                    floodTimerX1 = timer_read();
                 }
             }
         }
     }
 }
-
-
--- keyboards/keychron/q4_pro/ansi/keymaps/svarvat/matrix_scan.c
+++ keyboards/keychron/q4_pro/ansi/keymaps/svarvat/matrix_scan.c
@@ -23,6 +23,32 @@ extern bool isFloodDelOn;
 extern bool isFloodEntOn;
 extern bool isFloodBspcOn;
 
+static void move_mouse(int8_t x, int8_t y) {
+    report_mouse_t r = pointing_device_get_report();
+    r.x = x;
+    r.y = y;
+    pointing_device_set_report(r);
+    pointing_device_send();
+}
+
+static void scroll_mouse(int8_t v, int8_t h) {
+    report_mouse_t r = pointing_device_get_report();
+    r.v = v;
+    r.h = h;
+    pointing_device_set_report(r);
+    pointing_device_send();
+}
+
+static void dispatch_flood_keys(void) {
+    if (isFloodUpOn)    tap_code16(KC_UP);
+    if (isFloodDownOn)  tap_code16(KC_DOWN);
+    if (isFloodLeftOn)  tap_code16(KC_LEFT);
+    if (isFloodRightOn) tap_code16(KC_RIGHT);
+    if (isFloodDelOn)   tap_code16(KC_DEL);
+    if (isFloodEntOn)   tap_code16(KC_ENT);
+    if (isFloodBspcOn)  tap_code16(KC_BSPC);
+}
+
 void matrix_scan_user(void) {
     if (bt_wake_pending && timer_elapsed32(bt_wake_timer) > floodBlueToothInterval) {
         bt_wake_pending = false;
@@ -30,152 +56,58 @@ void matrix_scan_user(void) {
     }
     if (IS_LAYER_ON(LA_MOUSE)) {
         if (IS_LAYER_ON(LA_LTHUMBEMO)) {
-            if (isMouseRightOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = 8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (isMouseLeftOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = -8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-            if (isMouseDownOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = 8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (isMouseUpOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = -8;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-            if (isScrollDownOn || isScrollUpOn) {
+            int8_t mx = isMouseRightOn ? 8 : (isMouseLeftOn ? -8 : 0);
+            int8_t my = isMouseDownOn  ? 8 : (isMouseUpOn   ? -8 : 0);
+            if (mx || my) move_mouse(mx, my);
+
+            if (isScrollDownOn || isScrollUpOn || isScrollLeftOn || isScrollRightOn) {
                 if (timer_elapsed(floodTimerX3) > floodScrollIntervalX3) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.v = isScrollUpOn ? 20 : -20;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    floodTimerX3 = timer_read();
-                }
-            }
-            if (isScrollLeftOn || isScrollRightOn) {
-                if (timer_elapsed(floodTimerX3) > floodScrollIntervalX3) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.h = isScrollLeftOn ? -20 : 20;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
+                    scroll_mouse(isScrollUpOn   ? 20 : (isScrollDownOn  ? -20 : 0),
+                                 isScrollRightOn ? 20 : (isScrollLeftOn ? -20 : 0));
                     floodTimerX3 = timer_read();
                 }
             }
             if (isFloodUpOn || isFloodDownOn || isFloodLeftOn || isFloodRightOn
             || isFloodDelOn || isFloodEntOn || isFloodBspcOn) {
                 if (timer_elapsed(floodTimerX3) > floodKeyCodeIntervalX3) {
-                    if (isFloodUpOn)    tap_code16(KC_UP);
-                    if (isFloodDownOn)  tap_code16(KC_DOWN);
-                    if (isFloodLeftOn)  tap_code16(KC_LEFT);
-                    if (isFloodRightOn) tap_code16(KC_RIGHT);
-                    if (isFloodDelOn)   tap_code16(KC_DEL);
-                    if (isFloodEntOn)   tap_code16(KC_ENT);
-                    if (isFloodBspcOn)  tap_code16(KC_BSPC);
+                    dispatch_flood_keys();
                     floodTimerX3 = timer_read();
                 }
             }
         } else if (IS_LAYER_ON(LA_LTHUMBDMO)) {
-            if (isMouseDownOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = 3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (isMouseUpOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.y = -3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-            if (isMouseRightOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = 3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            } else if (isMouseLeftOn) {
-                report_mouse_t currentReport = pointing_device_get_report();
-                currentReport.x = -3;
-                pointing_device_set_report(currentReport);
-                pointing_device_send();
-            }
-            if (isScrollDownOn || isScrollUpOn) {
+            int8_t mx = isMouseRightOn ? 3 : (isMouseLeftOn ? -3 : 0);
+            int8_t my = isMouseDownOn  ? 3 : (isMouseUpOn   ? -3 : 0);
+            if (mx || my) move_mouse(mx, my);
+
+            if (isScrollDownOn || isScrollUpOn || isScrollLeftOn || isScrollRightOn) {
                 if (timer_elapsed(floodTimerX2) > floodScrollIntervalX2) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.v = isScrollUpOn ? 10 : -10;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    floodTimerX2 = timer_read();
-                }
-            }
-            if (isScrollLeftOn || isScrollRightOn) {
-                if (timer_elapsed(floodTimerX2) > floodScrollIntervalX2) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.h = isScrollLeftOn ? -10 : 10;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
+                    scroll_mouse(isScrollUpOn   ? 10 : (isScrollDownOn  ? -10 : 0),
+                                 isScrollRightOn ? 10 : (isScrollLeftOn ? -10 : 0));
                     floodTimerX2 = timer_read();
                 }
             }
             if (isFloodUpOn || isFloodDownOn || isFloodLeftOn || isFloodRightOn
             || isFloodDelOn || isFloodEntOn || isFloodBspcOn) {
                 if (timer_elapsed(floodTimerX2) > floodKeyCodeIntervalX2) {
-                    if (isFloodUpOn)    tap_code16(KC_UP);
-                    if (isFloodDownOn)  tap_code16(KC_DOWN);
-                    if (isFloodLeftOn)  tap_code16(KC_LEFT);
-                    if (isFloodRightOn) tap_code16(KC_RIGHT);
-                    if (isFloodDelOn)   tap_code16(KC_DEL);
-                    if (isFloodEntOn)   tap_code16(KC_ENT);
-                    if (isFloodBspcOn)  tap_code16(KC_BSPC);
+                    dispatch_flood_keys();
                     floodTimerX2 = timer_read();
                 }
             }
         } else {
-            if (isMouseDownOn || isMouseUpOn) {
+            if (isMouseRightOn || isMouseLeftOn || isMouseDownOn || isMouseUpOn) {
                 if (timer_elapsed(floodTimerX1) > floodMouseIntervalX1) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.y = isMouseDownOn ? 1 : -1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
+                    move_mouse(isMouseRightOn ? 1 : (isMouseLeftOn ? -1 : 0),
+                               isMouseDownOn  ? 1 : (isMouseUpOn   ? -1 : 0));
                     floodTimerX1 = timer_read();
                 }
             }
-            if (isMouseRightOn || isMouseLeftOn) {
-                if (timer_elapsed(floodTimerX1) > floodMouseIntervalX1) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.x = isMouseRightOn ? 1 : -1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    floodTimerX1 = timer_read();
-                }
-            }
-            if (isScrollDownOn || isScrollUpOn) {
+            if (isScrollDownOn || isScrollUpOn || isScrollLeftOn || isScrollRightOn) {
                 if (timer_elapsed(floodTimerX1) > floodScrollIntervalX1) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.v = isScrollUpOn ? 1 : -1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
-                    floodTimerX1 = timer_read();
-                }
-            }
-            if (isScrollLeftOn || isScrollRightOn) {
-                if (timer_elapsed(floodTimerX1) > floodScrollIntervalX1) {
-                    report_mouse_t currentReport = pointing_device_get_report();
-                    currentReport.h = isScrollLeftOn ? -1 : 1;
-                    pointing_device_set_report(currentReport);
-                    pointing_device_send();
+                    scroll_mouse(isScrollUpOn   ? 1 : (isScrollDownOn  ? -1 : 0),
+                                 isScrollRightOn ? 1 : (isScrollLeftOn ? -1 : 0));
                     floodTimerX1 = timer_read();
                 }
             }
         }
     }
 }
-
-
